3c53862a307fd3d8563ce2bf9ac27dfc3a296497
Cristian Sosa <gustavosocris@gmail.com>
2023-11-10 16:55:07 -0300

    test

--- src/app/core/pages/main/main.component.ts
+++ src/app/core/pages/main/main.component.ts
@@ -41,7 +41,7 @@ export class MainComponent implements OnInit {
 
   generateDynamicHTML = () => {
     navigator.bluetooth
-      .requestDevice({ filters: [{ services: ['printer'] }] })
+      .requestDevice({ filters: [{ services: ['SOL58_E437'] }] })
       .then((device) => {
         this.toastService.setToastState(true, device.id);
 
