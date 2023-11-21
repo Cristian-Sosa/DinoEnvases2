bdfcc5234cf8b9ff286152af16f6893b369dfcb7
Cristian Sosa <gustavosocris@gmail.com>
2023-11-21 12:25:50 -0300

    asdasdada

--- src/app/core/pages/main/main.component.ts
+++ src/app/core/pages/main/main.component.ts
@@ -84,7 +84,7 @@ export class MainComponent implements OnInit {
     const date = DateTime.now();
 
     this.cargaEnvaseService.observableEnvases().subscribe((envases) => {
-      this.cargaToPrint = `$small$SUPER MAMI$small$` + '\n';
+      this.cargaToPrint = `[C]<font size='small'>SUPER MAMI</font>` + '\n';
       // this.cargaToPrint = `\nSUPER MAMI\n`;
       // this.cargaToPrint += `VALE PARA ENVASE\n\n`;
 
